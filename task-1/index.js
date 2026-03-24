7a1f6d21f1de04133aa09e5a303e376b4f831bbe
ananyahegde <ananyahegde533@gmail.com>
2026-03-24 13:08:11 +0530

    store tasks in local storage

--- task-1/index.js
+++ task-1/index.js
@@ -6,21 +6,58 @@ window.onload = function() {
   inputText.value = '';
 }
 
+let tasks = [];
+
+// get tasks array from local storage if exists
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+console.log(tasks);
+
+// load initial tasks 
+for (i = 0; i < tasks.length; ++i) {
+  const li = document.createElement("li");
+  
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox"); 
+  input.id = i;
+
+  const label = document.createElement("label");
+  label.htmlFor = i;
+  
+  const button = document.createElement("button");
+
+  label.textContent = tasks[i];
+  button.innerHTML = "Remove";
+  
+  button.className = "delete";
+  button.onclick = function() { removeTodo(this); }; 
+
+  li.appendChild(input);
+  li.appendChild(label);
+  li.appendChild(button);
+  
+  listContainer.appendChild(li);
+}
+
 // add a todo task
 function addTodo() {
   if (inputText.value === '') {
     alert("Please enter a task name.");
   } else {
-      console.log("Im here 1");
       const li = document.createElement("li");
       
       const input = document.createElement("input");
       input.setAttribute("type", "checkbox"); 
-      
+      input.id = tasks.length;
+
       const label = document.createElement("label");
       const button = document.createElement("button");
 
       label.textContent = inputText.value;
+      label.htmlFor = tasks.length;
+
       button.innerHTML = "Remove";
       
       button.className = "delete";
@@ -31,6 +68,12 @@ function addTodo() {
       li.appendChild(button);
       
       listContainer.appendChild(li);
+      
+      // store the task in the array and save that to local storage
+      tasks.push(inputText.value);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      
+      inputText.value = '';
   }
 }
 
@@ -39,6 +82,13 @@ function removeTodo(btn) {
   if (confirm("Remove Task? This action cannot be undone.") == true) {
     btn.parentElement.remove();
     alert("Task removed.");
+    
+    const li = btn.parentElement;
+    const checkbox = li.querySelector("input");
+    const id = checkbox.id;
+
+    tasks.splice(id, 1);
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 }
-
