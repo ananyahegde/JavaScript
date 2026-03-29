4e146ef82007d2e8e7ccb75d6d006884f630283a
ananyahegde <ananyahegde533@gmail.com>
2026-03-29 18:53:24 +0530

    simulate replies with setTimeOut() function

--- task-7/index.js
+++ task-7/index.js
@@ -1,6 +1,24 @@
 inputText = document.getElementById("input-message");
 chatBody = document.querySelector(".chat-body");
 
+const replies = [
+  "Haha no way!",
+  "That's so cool!",
+  "Omg really?",
+  "Tell me more!",
+  "I had no idea!",
+  "Wait what?!",
+  "Lol same honestly",
+  "That's hilarious",
+  "No way that happened",
+  "I can't believe it!",
+  "Sounds fun!",
+  "Miss you too!",
+  "We should hang out soon",
+  "Agreed 100%",
+  "You're so right about that"
+];
+
 window.onload = function() {
   inputText.value = '';
 }
@@ -31,5 +49,27 @@ function sendMessage(e) {
     textSent.appendChild(timestamp);
     
     inputText.value = '';
+    
+    setTimeout(() => {
+      replyMessage = replies[Math.floor(Math.random() * 15)]
+      replyTime = new Date().toLocaleTimeString('en-in', { hour12: true, 
+                                             hour: "numeric", 
+                                             minute: "numeric"}).toUpperCase();
+
+    replyDiv = document.createElement('div');
+    replyText = document.createElement('p');
+    replyTimestamp = document.createElement('p');
+
+    replyText.textContent = replyMessage;
+    replyTimestamp.textContent = replyTime;
+
+    replyDiv.classList.add("chat-div-left");
+    replyText.classList.add("text");
+    replyTimestamp.classList.add("timestamp");
+    
+    chatBody.appendChild(replyDiv);
+    replyDiv.appendChild(replyText);
+    replyText.appendChild(replyTimestamp);
+    }, 2500);
   }
 }
