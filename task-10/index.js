ce9cb6b3623286bb945c753462037b605b320f69
ananyahegde <ananyahegde533@gmail.com>
2026-03-31 20:05:16 +0530

    implement filter by category and search by keyword

--- task-10/index.js
+++ task-10/index.js
@@ -3,14 +3,29 @@ import getCartData from './modules/cart.js';
 
 const sections = document.querySelectorAll(".section");
 const browseSection = document.getElementById("browse");
+const productFilter = document.getElementById("product-filter");
+const productSearch = document.getElementById("product-search");
 
+let products = [];
+
+getProductData().then((data) => {
+  products = data;
+  loadProducts(data);
+});
 
 window.onload = function() {
   history.replaceState(null, null, ' ');
+  document.getElementById("product-filter").value = 'All';
+  document.getElementById("product-search").value = '';
 }
 
-function loadProducts(product) {
-  product.forEach((productData) => {
+function loadProducts(products) {
+  console.log("Loading...");
+
+  const productsContainer = document.getElementById('product-container');
+  productsContainer.innerHTML = '';
+
+  products.forEach((productData) => {
     const card = document.createElement('div');
     card.classList.add('product-card-browse');
 
@@ -43,14 +58,52 @@ function loadProducts(product) {
     controls.append(minus, quantity, plus);
     card.append(img, name, description, price, controls);
     
-    browseSection.appendChild(card);
+    productsContainer.appendChild(card);
   });
 }
+
 
 function loadCart() {
 
 }
 
+productFilter.addEventListener("change", (event) => {
+  const val = document.getElementById("product-filter").value;
+  console.log(val);
+
+  if (val == "All") {
+    console.log("trueeee");
+    loadProducts(products);
+  } else {
+    const filteredProduct = products.filter(item => item.category === val);
+    loadProducts(filteredProduct);
+  }
+});
+
+productSearch.addEventListener("keypress", function(event) {
+  if (event.key === 'Enter') {
+    console.log(productSearch.value);
+
+    const filteredProduct = products.filter(item => 
+      item.name.toLowerCase().includes(productSearch.value.toLowerCase()) ||
+      item.description.toLowerCase().includes(productSearch.value.toLowerCase()) ||
+      item.category.toLowerCase().includes(productSearch.value.toLowerCase())
+    );
+    
+    if (filteredProduct.length != 0) {
+      loadProducts(filteredProduct);
+    } else {
+      const productsContainer = document.getElementById('product-container');
+      productsContainer.innerHTML = '';
+
+      const message = document.createElement("h3");
+      message.classList.add('err-message');
+      message.textContent = `Couldn't Find Product '${productSearch.value}'`;
+      
+      productsContainer.appendChild(message);
+    }
+  }
+});
 
 window.addEventListener("hashchange", () => {
   const target = (location.hash || "#home").substring(1);
@@ -61,8 +114,4 @@ window.addEventListener("hashchange", () => {
 
   const page = document.getElementById(target);
   page.style.display = "block";
-});
-
-getProductData().then((data) => {
-  loadProducts(data);
 });
