ecf1facf1ee1ef682b8772f14d2ccb69a2126b32
ananyahegde <ananyahegde533@gmail.com>
2026-04-01 13:39:47 +0530

    display cart with total amount breakdown

--- task-10/index.js
+++ task-10/index.js
@@ -2,6 +2,7 @@ import getProductData from './modules/product.js';
 
 const sections = document.querySelectorAll(".section");
 const browseSection = document.getElementById("browse");
+const cartSection = document.getElementById("cart");
 const productFilter = document.getElementById("product-filter");
 const productSearch = document.getElementById("product-search");
 
@@ -13,6 +14,9 @@ getProductData().then((data) => {
   loadProducts(data);
 });
 
+// call load cart here 
+loadCart();
+
 window.onload = function() {
   history.replaceState(null, null, ' ');
   document.getElementById("product-filter").value = 'All';
@@ -20,8 +24,8 @@ window.onload = function() {
 }
 
 function loadProducts(products) {
-  const productsContainer = document.getElementById('product-container');
-  productsContainer.innerHTML = '';
+  const productContainer = document.getElementById('product-container');
+  productContainer.innerHTML = '';
 
   products.forEach((productData) => {
     const card = document.createElement('div');
@@ -58,7 +62,7 @@ function loadProducts(products) {
       if(cart.find(item => item.id === productData.id)) {
         cart.find(item => item.id === productData.id).quantity += 1;
       } else {
-        cart.push({id: productData.id, quantity: 1})
+        cart.push({id: productData.id, image: productData.image, name: productData.name, price: productData.price, quantity: 1})
       }
       
       try {
@@ -99,12 +103,109 @@ function loadProducts(products) {
     controls.append(minus, quantity, plus);
     card.append(img, name, description, price, controls);
     
-    productsContainer.appendChild(card);
+    productContainer.appendChild(card);
   });
 }
 
 function loadCart() {
+  cart = JSON.parse(localStorage.getItem("cartInfo")) || [];
+  
+  if (!cart || cart.length == 0) {
+    const emptyMsg = document.createElement("p");
+    
+    emptyMsg.textContent = "Your cart is empty.";
+    emptyMsg.classList.add('empty-msg');
+ 
+    cartSection.appendChild(emptyMsg);
+  } else {
+    const cartContainer = document.createElement("div");
+    cartContainer.classList.add("cart-container");
+    
+    cart.forEach((item) => {
+      const cartItem = document.createElement("div");
+      cartItem.classList.add("cart-item");
 
+      const img = document.createElement("img");
+      
+      const cartItemInfo = document.createElement("div");
+      cartItemInfo.classList.add("cart-item-info");
+      
+      const name = document.createElement("p");
+      
+      const price = document.createElement("p");
+      price.classList.add("item-price-quantity");
+      
+      const quantity = document.createElement("p");
+      quantity.classList.add("item-price-quantity");
+
+      img.src = item.image;
+      img.alt = item.name;
+      name.textContent = item.name;
+      price.textContent = `₹${item.price.toLocaleString('en-IN')}`;
+      quantity.textContent = `Quantity: ${item.quantity}`;
+
+      cartItemInfo.append(name, quantity, price);
+      cartItem.append(img, cartItemInfo);
+      cartContainer.appendChild(cartItem);
+    });
+    const totalContainer = document.createElement("div");
+
+    const subTotalEl = document.createElement("p");
+    const discountEl = document.createElement("p");
+    const taxEl = document.createElement("p");
+    const grandTotalEl = document.createElement("p");
+
+    let total = 0;
+    cart.forEach((item) => {
+      total += Number(item.price) * Number(item.quantity);
+    });
+    
+    subTotalEl.classList.add('subtotal');
+    subTotalEl.innerHTML = `<span>Subtotal</span><span>₹${total.toFixed(2)}</span>`;
+    
+    let discount = 0; 
+    
+    if(total > 4999) {
+      discount = 0.15;
+    } else if (total > 2999) {
+      discount = 0.1;
+    } else if (total > 1999) {
+      discount = 0.05;
+    } else {
+      discount = 0;
+    }
+
+    let discountAmt = 0; 
+
+    if(discount) {
+      discountAmt = total * discount;
+    }
+
+    discountEl.innerHTML = `<span>Discount</span><span>₹${discountAmt.toFixed(2)}</span>`;
+
+    let tax;
+
+    if (total > 2500) {
+      tax = 0.18;
+    } else if (total >  1000) {
+      tax = 0.12;
+    } else {
+      tax = 0.05;
+    }
+
+    const taxAmt = (total - discountAmt) * tax;
+    taxEl.innerHTML = `<span>Tax (${tax*100}%)</span><span>₹${taxAmt.toFixed(2)}</span>`;
+    
+    const grandTotal = total - discountAmt + taxAmt;
+
+    grandTotalEl.classList.add('grand-total');
+    grandTotalEl.innerHTML = `<span>Grand Total</span><span>₹${grandTotal.toFixed(2)}</span>`;
+
+    totalContainer.classList.add("total-container");
+    totalContainer.append(subTotalEl, discountEl, taxEl, grandTotalEl);
+    
+    cartSection.append(cartContainer, totalContainer);
+  }
 }
 
 productFilter.addEventListener("change", (event) => {
@@ -128,14 +229,14 @@ productSearch.addEventListener("keypress", function(event) {
     if (filteredProduct.length != 0) {
       loadProducts(filteredProduct);
     } else {
-      const productsContainer = document.getElementById('product-container');
-      productsContainer.innerHTML = '';
+      const productContainer = document.getElementById('product-container');
+      productContainer.innerHTML = '';
 
-      const message = document.createElement("h3");
+      const message = document.createElement("p");
       message.classList.add('err-message');
       message.textContent = `Couldn't Find Product '${productSearch.value}'`;
 
-      productsContainer.appendChild(message);
+      productContainer.appendChild(message);
     }
   }
 });
